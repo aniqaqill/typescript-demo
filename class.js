18078726a3483133d4eeffaa51da78118f65e864
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-08-05 00:48:59 +0800

    test

--- class.js
+++ class.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
     if (kind === "m") throw new TypeError("Private method is not writable");
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
@@ -11,41 +26,47 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
 var _employee_id;
-Object.defineProperty(exports, "__esModule", { value: true });
-class Person {
-    constructor(name, age) {
+exports.__esModule = true;
+var Person = /** @class */ (function () {
+    function Person(name, age) {
         this.name = name;
         this.age = age;
     }
-    getNamewithAge() {
-        return `${this.name} is ${this.age} years old`;
-    }
-    greet() {
-        console.log(`Hello ${this.name}`);
-    }
-}
-let person1 = new Person("Aniq", 20);
+    Person.prototype.getNamewithAge = function () {
+        return "".concat(this.name, " is ").concat(this.age, " years old");
+    };
+    Person.prototype.greet = function () {
+        console.log("Hello ".concat(this.name));
+    };
+    return Person;
+}());
+var person1 = new Person("Aniq", 20);
 person1.greet();
-class employee {
-    constructor(id, name, adress) {
+var employee = /** @class */ (function () {
+    function employee(id, name, adress) {
         _employee_id.set(this, void 0);
         __classPrivateFieldSet(this, _employee_id, id, "f");
         this.name = name;
         this.adress = adress;
     }
-    set empId(id) {
-        __classPrivateFieldSet(this, _employee_id, id, "f");
-    }
-    get empId() {
-        return __classPrivateFieldSet(this, _employee_id, __classPrivateFieldGet(this, _employee_id, "f"), "f");
-    }
-    getNamewithAdress() {
-        return `${this.name} stays at ${this.adress}`;
-    }
-}
+    Object.defineProperty(employee.prototype, "empId", {
+        get: function () {
+            return __classPrivateFieldSet(this, _employee_id, __classPrivateFieldGet(this, _employee_id, "f"), "f");
+        },
+        set: function (id) {
+            __classPrivateFieldSet(this, _employee_id, id, "f");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    employee.prototype.getNamewithAdress = function () {
+        return "".concat(this.name, " stays at ").concat(this.adress);
+    };
+    return employee;
+}());
 _employee_id = new WeakMap();
-let emp1 = new employee(1, "Aniq", "Jalan 1");
-let emp2 = new employee(2, "Aniq", "Jalan 2");
+var emp1 = new employee(1, "Aniq", "Jalan 1");
+var emp2 = new employee(2, "Aniq", "Jalan 2");
 emp1.empId = 3;
 console.log(emp1);
 console.log(emp2.getNamewithAdress());
@@ -53,15 +74,17 @@ console.log(emp1.empId);
 //multiple constructor in class is not allowed
 //inheritance
 //inheritance is a way to extend a class
-class Programmer extends Person {
-    constructor(name, age) {
-        super(name, age);
+var Programmer = /** @class */ (function (_super) {
+    __extends(Programmer, _super);
+    function Programmer(name, age) {
+        return _super.call(this, name, age) || this;
     }
-    greet() {
-        console.log(`Hello ${this.name} from Programmer class`);
-    }
-}
-let prog1 = new Programmer("Aniq", 20);
+    Programmer.prototype.greet = function () {
+        console.log("Hello ".concat(this.name, " from Programmer class"));
+    };
+    return Programmer;
+}(Person));
+var prog1 = new Programmer("Aniq", 20);
 prog1.greet();
 console.log(prog1.getNamewithAge());
 //abstract class
--- interface.js
+++ interface.js
@@ -1,7 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let { name: userName, email: userLogin } = { name: "John", age: 25, id: 1, email: "  " };
-let employee2 = { name: "John", age: 25, id: 1, email: "  ", salary: 1000 };
+exports.__esModule = true;
+var _a = { name: "John", age: 25, id: 1, email: "  " }, userName = _a.name, userLogin = _a.email;
+var employee2 = { name: "John", age: 25, id: 1, email: "  ", salary: 1000 };
 // let users : User[] = [
 //     { name: "John", age: 25, id: 1, email: "  " },
 //     { name: "Jane", age: 25, id: 2, email: "  " },
@@ -9,12 +9,12 @@ let employee2 = { name: "John", age: 25, id: 1, email: "  ", salary: 1000 };
 // ];
 // console.log(users[0].name)
 // console.log(users[1].name)
-let [user1, user2, ...restUsers] = [
+var _b = [
     { name: "John", age: 25, id: 1, email: "  " },
     { name: "Jane", age: 25, id: 2, email: "  " },
     { name: "Bob", age: 25, id: 3, email: "  " },
     { name: "Aniq", age: 20, id: 4, email: "  " }
-];
+], user1 = _b[0], user2 = _b[1], restUsers = _b.slice(2);
 console.log(user1.name);
 console.log(user2.name);
 console.log(restUsers);
