857ecb1ba2115111e8e553e58337e62909c63596
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-08-29 18:33:14 +0800

    function explaination

---
+++ myfunctions.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//this function will typecheck the input and also return type
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addTwoNumbers(1, 2));
---
+++ myfunctions.ts
@@ -0,0 +1,11 @@
+
+//this function will typecheck the input and also return type
+function addTwoNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+
+
+console.log(addTwoNumbers(1, 2));
+
+export {};
